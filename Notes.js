cf118d4d86bc9ed8a992adc03e43f4776a9c768e
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2026-02-15 21:13:01 -0500

    add basic Node.js commands

---
+++ Notes.js
@@ -0,0 +1,7 @@
+/* Commands
+node: start the node envirment
+.exit, control+D: stop the envirment
+[TAB]: list all the variable
+  if we add things like "String." we can see all the available methods
+_: this mean the previous result of the calculation
+*/
