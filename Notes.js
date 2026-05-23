1676089da8b094d8b1aca47bb9e61c6df2a2957c
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2026-05-22 21:14:14 -0400

    addtional notes on code execution

--- Notes.js
+++ Notes.js
@@ -1,5 +1,6 @@
 /* Commands
 node: start the node envirment
+  add the file name after will run the fileds
 .exit, control+D: stop the envirment
 [TAB]: list all the variable
   if we add things like "String." we can see all the available methods
